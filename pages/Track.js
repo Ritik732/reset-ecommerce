e7db4f9d49fd4f7adb2d5d662db28fd15d937056
Ritik732 <guptaritik23364@gmail.com>
2023-02-01 10:01:38 +0530

    uploaded files

--- pages/Track.js
+++ pages/Track.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Track() {
   return (
-    <div className='container mx-auto px-4'>Track</div>
+    <div className='container mx-auto px-4'>Track  </div>
   )
 }
 
